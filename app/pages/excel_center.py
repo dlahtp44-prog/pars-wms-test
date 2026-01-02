159f9d464eba98483a1ec70e62c8578997134133
dlahtp44-prog <dlahtp44@gmail.com>
2026-01-02 15:31:09 +0900

    Update excel_center.py

--- app/pages/excel_center.py
+++ app/pages/excel_center.py
@@ -1,14 +1,12 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from app.core.paths import TEMPLATES_DIR
+router = APIRouter(prefix="/page", tags=["Excel Center"])
+templates = Jinja2Templates(directory="app/templates")
 
-
-router = APIRouter(prefix="/page/excel", tags=["page-excel-center"])
-templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
-
-
-@router.get("", response_class=HTMLResponse)
+@router.get("/excel")
 def excel_center(request: Request):
-    return templates.TemplateResponse("excel_center.html", {"request": request})
+    return templates.TemplateResponse(
+        "excel_center.html",
+        {"request": request}
+    )
