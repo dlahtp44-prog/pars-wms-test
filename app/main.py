29322beb832b95ab439c79970bcda96bdb171abe
dlahtp44-prog <dlahtp44@gmail.com>
2026-01-02 15:27:26 +0900

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -12,6 +12,8 @@ from app.pages.move import router as move_page_router
 from app.pages.inventory import router as inventory_page_router
 from app.pages.history import router as history_page_router
 from app.pages.excel_outbound import router as excel_outbound_page_router
+from app.pages.excel_inbound import router as excel_inbound_page_router
+from app.pages.excel_center import router as excel_center_page_router
 
 # mobile pages
 from app.pages.mobile_home import router as mobile_home_router
@@ -27,8 +29,9 @@ from app.routers.api_move import router as api_move_router
 from app.routers.api_inventory import router as api_inventory_router
 from app.routers.api_history import router as api_history_router
 from app.routers.excel_outbound import router as api_excel_outbound_router
+from app.routers.excel_inbound import router as api_excel_inbound_router
 
-app = FastAPI(title="PARS WMS", version="1.6.4")
+app = FastAPI(title="PARS WMS", version="1.6.5-stable")
 
 @app.on_event("startup")
 def _startup():
@@ -46,6 +49,8 @@ app.include_router(move_page_router)
 app.include_router(inventory_page_router)
 app.include_router(history_page_router)
 app.include_router(excel_outbound_page_router)
+app.include_router(excel_inbound_page_router)
+app.include_router(excel_center_page_router)
 
 app.include_router(mobile_home_router)
 app.include_router(mobile_qr_router)
@@ -59,3 +64,4 @@ app.include_router(api_move_router)
 app.include_router(api_inventory_router)
 app.include_router(api_history_router)
 app.include_router(api_excel_outbound_router)
+app.include_router(api_excel_inbound_router)
