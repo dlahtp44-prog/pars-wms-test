7693cb06a44a8ee29bb61e93dc150dc0684fa0cf
dlahtp44-prog <dlahtp44@gmail.com>
2026-01-05 13:07:59 +0900

    Update move.py

--- app/pages/move.py
+++ app/pages/move.py
@@ -5,7 +5,6 @@ router = APIRouter(prefix="/page/move", tags=["page-move"])
 
 @router.get("", response_class=HTMLResponse)
 def page(request: Request):
-    # main.py에서 설정한 templates 엔진을 그대로 사용
     return request.app.state.templates.TemplateResponse(
         "move.html",
         {"request": request}
